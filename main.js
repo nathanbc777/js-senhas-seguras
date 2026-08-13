9e9c0a1de2ee5873b955983eceb213168a49736e
nathanbc777 <nathan.castanha@escola.pr.gov.br>
2026-08-12 22:51:08 -0300

    aula.12/08

--- main.js
+++ main.js
@@ -22,15 +22,38 @@ function aumentaTamanho() {
     geraSenha() 
 }
 
-const campoSenha = document.querySelector('campo-senha');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+
+for (i = 0, i < checkbox.length; i++) {
+    checkbox[i].onclick = geraSenha; 
+}
 
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-geraSenha() {
-    let senha = '';
-    for (let i=0; i< tamanhoSenha; i++) {
-         let numeroAleatorio = Math.random() * letrasMaiusculas.length;
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+geraSenha();
+
+    function geraSenha() {
+        let alfabeto = '';
+        if (checkbox[0].checked) {
+            alfabeto = alfabeto + letrasMaiusculas;
+        }
+        if (checkbox[1].checked) {
+            alfabeto = alfabeto + letrasMinusculas;
+        }
+        if (checkbox[2].checked) {
+            alfabeto = alfabeto + numeros;
+        }
+        if (checkbox[3].checked) {
+            alfabeto = alfabeto + simboos;
+        }
+        let senha = '';
+        for (let i=0; i< tamanhoSenha; i++) {
+         let numeroAleatorio = Math.random() * alfabeto.length;
          numeroAleatorio = Math.floor(numeroAleatorio);
-         senha = senha + letrasMaiusculas[numeroAleatorio];
+         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
 }
